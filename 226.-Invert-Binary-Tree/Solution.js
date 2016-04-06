394513a48bbb007066a02b36b8b189720c24be5c
Yida Chen <yida820310@gmail.com>
2016-04-06 21:41:30 +0800

    Create Solution.js

---
+++ 226.-Invert-Binary-Tree/Solution.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if(root !== null){
+        var temp = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(temp);
+    }
+    return root;
+};
