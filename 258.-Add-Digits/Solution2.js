b35ae63cd203de217f34f2c856f6e3cb27d145d2
Yida Chen <yida820310@gmail.com>
2016-04-06 20:36:20 +0800

    Update Solution2.js

--- 258.-Add-Digits/Solution2.js
+++ 258.-Add-Digits/Solution2.js
@@ -11,3 +11,29 @@ var addDigits = function(num) {
     }
     return num % 9;
 };
+
+/* https://leetcode.com/discuss/94329/simple-java-solution-no-recursion-loop
+I'll try to explain the math behind this:
+
+First you should understand:
+
+10^k % 9 = 1
+a*10^k % 9 = a % 9 
+Then let's use an example to help explain.
+
+Say a number x = 23456
+
+x = 2* 10000 + 3 * 1000 + 4 * 100 + 5 * 10 + 6
+
+2 * 10000 % 9 = 2 % 9
+
+3 * 1000 % 9 = 3 % 9
+
+4 * 100 % 9 = 4 % 9
+
+5 * 10 % 9 = 5 % 9
+
+Then x % 9 = ( 2+ 3 + 4 + 5 + 6) % 9, note that x = 2* 10000 + 3 * 1000 + 4 * 100 + 5 * 10 + 6
+
+So we have 23456 % 9 = (2 + 3 + 4 + 5 + 6) % 9
+*/
