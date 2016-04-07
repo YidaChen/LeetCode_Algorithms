18a784853666c3d79b308579c95029a4f1692deb
Yida Chen <yida820310@gmail.com>
2016-04-07 14:35:57 +0800

    Create Solution2.js

---
+++ 169.-Majority-Element/Solution2.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    var major = nums[0];
+    var count = 1;
+    
+    for(var i = 1; i < nums.length; i++){
+        if(count === 0){
+            count++;
+            major=nums[i];
+        }else if(major === nums[i]){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    return major;
+};
+
+// https://leetcode.com/discuss/24971/o-n-time-o-1-space-fastest-solution
